b72689ab6e785acd5f852da56063d910c92f26ed
Ian York <iyork28@gmail.com>
2014-01-20 20:44:40 -0500

    Changed help and home in static_pages, added about; and tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home Page" do
+    it "should have the content of 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+  	it "should have the content 'Help'" do 
+  		visit '/static_pages/help'
+  		expect(page).to have_content('Help')
+  	end
+  end
+
+  describe "About page" do
+  	it "should have the content 'About Us'" do 
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About Us')
+  	end
+  end
+
+end
