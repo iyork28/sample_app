333d643e411570c9a427032fef21a44ed4070e47
Ian York <iyork28@gmail.com>
2014-01-23 20:47:49 -0500

    Added bootstrap support to several pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root 'static_pages#home'
+  
+  get "contact", to: 'static_pages#contact' 
+  get "help",    to: 'static_pages#help'
+  get "about",   to: 'static_pages#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
